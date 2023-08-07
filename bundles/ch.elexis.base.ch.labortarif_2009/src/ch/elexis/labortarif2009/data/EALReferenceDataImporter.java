b47d18766ab9f2ca894ffbb91110d2efbd6a51c4
rgwch <weirich@elexis.ch>
2023-08-07 11:18:56 +0200

    allow import of 2023 version of eal

--- bundles/ch.elexis.base.ch.labortarif_2009/src/ch/elexis/labortarif2009/data/EALReferenceDataImporter.java
+++ bundles/ch.elexis.base.ch.labortarif_2009/src/ch/elexis/labortarif2009/data/EALReferenceDataImporter.java
@@ -44,22 +44,23 @@ import ch.rgw.tools.TimeTool;
 
 public class EALReferenceDataImporter extends AbstractReferenceDataImporter {
 	private static final Logger logger = LoggerFactory.getLogger(EALReferenceDataImporter.class);
-	
+
 	int langdef = 0;
 	TimeTool validFrom;
-	
+
 	Fachspec[] specs;
 	int row;
 	// use local HashMap instead of creating new one for every tarif position
 	private HashMap<String, String> importedValues = new HashMap<String, String>();
 
 	@Override
-	public @NonNull Class<?> getReferenceDataTypeResponsibleFor(){
+	public @NonNull Class<?> getReferenceDataTypeResponsibleFor() {
 		return Labor2009Tarif.class;
 	}
-	
+
 	@Override
-	public IStatus performImport(@Nullable IProgressMonitor monitor, @NonNull InputStream input, @Nullable Integer newVersion){
+	public IStatus performImport(@Nullable IProgressMonitor monitor, @NonNull InputStream input,
+			@Nullable Integer newVersion) {
 		if (monitor == null) {
 			monitor = new NullProgressMonitor();
 		}
@@ -67,7 +68,7 @@ public class EALReferenceDataImporter extends AbstractReferenceDataImporter {
 		validFrom = getValidFromVersion(newVersion);
 
 		String lang = JdbcLink.wrap(CoreHub.localCfg.get( // d,f,i
-			Preferences.ABL_LANGUAGE, "d").toUpperCase()); //$NON-NLS-1$
+				Preferences.ABL_LANGUAGE, "d").toUpperCase()); //$NON-NLS-1$
 		if (lang.startsWith("F")) { //$NON-NLS-1$
 			langdef = 1;
 		} else if (lang.startsWith("I")) { //$NON-NLS-1$
@@ -76,37 +77,43 @@ public class EALReferenceDataImporter extends AbstractReferenceDataImporter {
 		specs = Importer.loadFachspecs(langdef);
 		if (specs != null) {
 			ExcelWrapper exw = new ExcelWrapper();
-			exw.setFieldTypes(new Class[] {
-				String.class /* chapter */, String.class /* rev */, String.class /* code */,
-				String.class /* tp */, String.class /* name */, String.class /* lim */,
-				String.class /* fach (2011) comment (2012) */, String.class
-			/* fach (2012) */
-			});
+			Class<?>[] fields=new Class[30];
+			for(int i=0;i<fields.length;i++) {
+				fields[i]=String.class;
+			}
+			exw.setFieldTypes(fields);
 			if (exw.load(input, langdef)) {
 				int first = exw.getFirstRow();
 				int last = exw.getLastRow();
 				int count = last - first;
 				if (monitor != null)
 					monitor.beginTask(Messages.Importer_importEAL, count);
-				
+
 				String[] line = exw.getRow(1).toArray(new String[0]);
 				// determine format of file according to year of tarif
 				int formatYear = getFormatYear(line);
-				if (formatYear != 2011 && formatYear != 2012 && formatYear != 2018)
-					return new Status(Status.ERROR, "ch.elexis.labotarif.ch2009", //$NON-NLS-1$
-						"unknown file format"); //$NON-NLS-1$
-					
+
 				for (int i = first + 1; i <= last; i++) {
 					row = i;
 					line = exw.getRow(i).toArray(new String[0]);
-					
-					if (formatYear == 2011)
+					switch (formatYear) {
+					case 2011:
 						fillImportedValues2011(line);
-					else if (formatYear == 2012)
+						break;
+					case 2012:
 						fillImportedValues2012(line);
-					else if (formatYear == 2018)
+						break;
+					case 2018:
 						fillImportedValues2018(line);
-					
+						break;
+					case 2023:
+						fillImportedValues2023(line);
+						break;
+					default:
+						return new Status(Status.ERROR, "ch.elexis.labotarif.ch2009", //$NON-NLS-1$
+								"unknown file format"); //$NON-NLS-1$
+					}
+
 					if (importedValues.size() > 0) {
 						updateOrCreateFromImportedValues();
 					}
@@ -117,12 +124,12 @@ public class EALReferenceDataImporter extends AbstractReferenceDataImporter {
 						}
 					}
 				}
-				
+
 				closeAllOlder();
 				if (monitor != null)
 					monitor.done();
 				ElexisEventDispatcher.reload(Labor2009Tarif.class);
-				if(newVersion != null) {
+				if (newVersion != null) {
 					Labor2009Tarif.setCurrentCodeVersion(newVersion);
 				}
 				EALBlocksCodeUpdater blocksUpdater = new EALBlocksCodeUpdater();
@@ -132,10 +139,8 @@ public class EALReferenceDataImporter extends AbstractReferenceDataImporter {
 			}
 		}
 		return new Status(Status.ERROR, "ch.elexis.labotarif.ch2009", //$NON-NLS-1$
-			"could not load file"); //$NON-NLS-1$
+				"could not load file"); //$NON-NLS-1$
 	}
-	
-
 
 	/**
 	 * Convert version Integer in yymmdd format to date.
@@ -143,11 +148,10 @@ public class EALReferenceDataImporter extends AbstractReferenceDataImporter {
 	 * @param newVersion
 	 * @return
 	 */
-	private TimeTool getValidFromVersion(Integer newVersion){
+	private TimeTool getValidFromVersion(Integer newVersion) {
 		String intString = Integer.toString(newVersion);
 		if (intString.length() != 6) {
-			throw new IllegalStateException("Version " + newVersion
-				+ " can not be parsed to valid date.");
+			throw new IllegalStateException("Version " + newVersion + " can not be parsed to valid date.");
 		}
 		String year = intString.substring(0, 2);
 		String month = intString.substring(2, 4);
@@ -159,19 +163,19 @@ public class EALReferenceDataImporter extends AbstractReferenceDataImporter {
 		return ret;
 	}
 
-	public int getVersionFromValid(TimeTool validFrom){
+	public int getVersionFromValid(TimeTool validFrom) {
 		int year = validFrom.get(TimeTool.YEAR);
 		int month = validFrom.get(TimeTool.MONTH) + 1;
 		int day = validFrom.get(TimeTool.DAY_OF_MONTH);
-		
+
 		return day + (month * 100) + ((year - 2000) * 10000);
 	}
 
-	private void updateOrCreateFromImportedValues(){
+	private void updateOrCreateFromImportedValues() {
 		// get all entries with matching code
 		Query<Labor2009Tarif> qEntries = new Query<Labor2009Tarif>(Labor2009Tarif.class);
 		qEntries.add(Labor2009Tarif.FLD_CODE, "=", importedValues.get(Labor2009Tarif.FLD_CODE));
-		
+
 		List<Labor2009Tarif> entries = qEntries.execute();
 		List<Labor2009Tarif> openEntries = new ArrayList<Labor2009Tarif>();
 		// get open entries -> field FLD_GUELTIG_BIS not set
@@ -182,55 +186,43 @@ public class EALReferenceDataImporter extends AbstractReferenceDataImporter {
 		}
 		if (openEntries.isEmpty()) {
 			// just create if there are no open entries
-			Labor2009Tarif tarif =
-				new Labor2009Tarif(importedValues.get(Labor2009Tarif.FLD_CHAPTER),
-					importedValues.get(Labor2009Tarif.FLD_CODE),
-					importedValues.get(Labor2009Tarif.FLD_TP),
-					importedValues.get(Labor2009Tarif.FLD_NAME),
-					importedValues.get(Labor2009Tarif.FLD_LIMITATIO),
-					importedValues.get(Labor2009Tarif.FLD_FACHBEREICH), Fachspec.getFachspec(specs,
-						row));
+			Labor2009Tarif tarif = new Labor2009Tarif(importedValues.get(Labor2009Tarif.FLD_CHAPTER),
+					importedValues.get(Labor2009Tarif.FLD_CODE), importedValues.get(Labor2009Tarif.FLD_TP),
+					importedValues.get(Labor2009Tarif.FLD_NAME), importedValues.get(Labor2009Tarif.FLD_LIMITATIO),
+					importedValues.get(Labor2009Tarif.FLD_FACHBEREICH), Fachspec.getFachspec(specs, row));
 			tarif.set(Labor2009Tarif.FLD_GUELTIG_VON, validFrom.toString(TimeTool.DATE_COMPACT));
 		} else {
 			// do actual import if entries with updating open entries
 			for (Labor2009Tarif labor2009Tarif : openEntries) {
-				if (labor2009Tarif.get(Labor2009Tarif.FLD_GUELTIG_VON).equals(
-					validFrom.toString(TimeTool.DATE_COMPACT))) {
+				if (labor2009Tarif.get(Labor2009Tarif.FLD_GUELTIG_VON)
+						.equals(validFrom.toString(TimeTool.DATE_COMPACT))) {
 					// test if the gVon is the same -> update the values of the entry
-					labor2009Tarif.set(new String[] {
-						Labor2009Tarif.FLD_CHAPTER, Labor2009Tarif.FLD_CODE, Labor2009Tarif.FLD_TP,
-						Labor2009Tarif.FLD_NAME, Labor2009Tarif.FLD_LIMITATIO,
-						Labor2009Tarif.FLD_FACHBEREICH, Labor2009Tarif.FLD_FACHSPEC
-						},
-						concatChapter(labor2009Tarif,
-							importedValues.get(Labor2009Tarif.FLD_CHAPTER)),
-						importedValues.get(Labor2009Tarif.FLD_CODE),
-						importedValues.get(Labor2009Tarif.FLD_TP),
-						importedValues.get(Labor2009Tarif.FLD_NAME),
-						importedValues.get(Labor2009Tarif.FLD_LIMITATIO),
-						importedValues.get(Labor2009Tarif.FLD_FACHBEREICH),
-						Integer.toString(Fachspec.getFachspec(specs, row)));
-				} else {
-					// close entry and create new entry
-					labor2009Tarif.set(Labor2009Tarif.FLD_GUELTIG_BIS,
-						validFrom.toString(TimeTool.DATE_COMPACT));
-					
-					Labor2009Tarif tarif =
-						new Labor2009Tarif(importedValues.get(Labor2009Tarif.FLD_CHAPTER),
-							importedValues.get(Labor2009Tarif.FLD_CODE),
-							importedValues.get(Labor2009Tarif.FLD_TP),
+					labor2009Tarif.set(
+							new String[] { Labor2009Tarif.FLD_CHAPTER, Labor2009Tarif.FLD_CODE, Labor2009Tarif.FLD_TP,
+									Labor2009Tarif.FLD_NAME, Labor2009Tarif.FLD_LIMITATIO,
+									Labor2009Tarif.FLD_FACHBEREICH, Labor2009Tarif.FLD_FACHSPEC },
+							concatChapter(labor2009Tarif, importedValues.get(Labor2009Tarif.FLD_CHAPTER)),
+							importedValues.get(Labor2009Tarif.FLD_CODE), importedValues.get(Labor2009Tarif.FLD_TP),
 							importedValues.get(Labor2009Tarif.FLD_NAME),
 							importedValues.get(Labor2009Tarif.FLD_LIMITATIO),
 							importedValues.get(Labor2009Tarif.FLD_FACHBEREICH),
-							Fachspec.getFachspec(specs, row));
-					tarif.set(Labor2009Tarif.FLD_GUELTIG_VON,
-						validFrom.toString(TimeTool.DATE_COMPACT));
+							Integer.toString(Fachspec.getFachspec(specs, row)));
+				} else {
+					// close entry and create new entry
+					labor2009Tarif.set(Labor2009Tarif.FLD_GUELTIG_BIS, validFrom.toString(TimeTool.DATE_COMPACT));
+
+					Labor2009Tarif tarif = new Labor2009Tarif(importedValues.get(Labor2009Tarif.FLD_CHAPTER),
+							importedValues.get(Labor2009Tarif.FLD_CODE), importedValues.get(Labor2009Tarif.FLD_TP),
+							importedValues.get(Labor2009Tarif.FLD_NAME),
+							importedValues.get(Labor2009Tarif.FLD_LIMITATIO),
+							importedValues.get(Labor2009Tarif.FLD_FACHBEREICH), Fachspec.getFachspec(specs, row));
+					tarif.set(Labor2009Tarif.FLD_GUELTIG_VON, validFrom.toString(TimeTool.DATE_COMPACT));
 				}
 			}
 		}
 	}
 
-	private String concatChapter(Labor2009Tarif existing, String chapter){
+	private String concatChapter(Labor2009Tarif existing, String chapter) {
 		String existingChapter = existing.get(Labor2009Tarif.FLD_CHAPTER);
 		if (existingChapter != null && !existingChapter.isEmpty()) {
 			return chaptersMakeUnique(existingChapter + ", " + chapter);
@@ -238,8 +230,8 @@ public class EALReferenceDataImporter extends AbstractReferenceDataImporter {
 			return chaptersMakeUnique(chapter);
 		}
 	}
-	
-	private String chaptersMakeUnique(String chapters){
+
+	private String chaptersMakeUnique(String chapters) {
 		String[] parts = chapters.split(", ");
 		if (parts != null && parts.length > 1) {
 			StringBuilder sb = new StringBuilder();
@@ -259,77 +251,108 @@ public class EALReferenceDataImporter extends AbstractReferenceDataImporter {
 		}
 	}
 
-	private void fillImportedValues2012(String[] line){
+	private void fillImportedValues2023(String[] line) {
 		importedValues.clear();
-		importedValues.put(Labor2009Tarif.FLD_CHAPTER, StringTool.getSafe(line, 0));
-		// convert code to nnnn.mm
-		String code = convertCodeString(StringTool.getSafe(line, 2));
-		
-		importedValues.put(Labor2009Tarif.FLD_CODE, code);
-		importedValues.put(Labor2009Tarif.FLD_TP,
-			convertLocalizedNumericString(StringTool.getSafe(line, 3)).toString());
-		importedValues.put(Labor2009Tarif.FLD_NAME,
-			StringTool.limitLength(StringTool.getSafe(line, 4), 254));
-		importedValues.put(Labor2009Tarif.FLD_LIMITATIO, StringTool.getSafe(line, 5));
-		importedValues.put(Labor2009Tarif.FLD_FACHBEREICH, StringTool.getSafe(line, 7));
-	}
-	
-	private void fillImportedValues2011(String[] line){
-		importedValues.clear();
-		importedValues.put(Labor2009Tarif.FLD_CHAPTER, StringTool.getSafe(line, 0));
-		// convert code to nnnn.mm
-		String code = convertCodeString(StringTool.getSafe(line, 2));
-		
-		importedValues.put(Labor2009Tarif.FLD_CODE, code);
-		importedValues.put(Labor2009Tarif.FLD_TP,
-			convertLocalizedNumericString(StringTool.getSafe(line, 3)).toString());
-		importedValues.put(Labor2009Tarif.FLD_NAME,
-			StringTool.limitLength(StringTool.getSafe(line, 4), 254));
-		importedValues.put(Labor2009Tarif.FLD_LIMITATIO, StringTool.getSafe(line, 5));
-		importedValues.put(Labor2009Tarif.FLD_FACHBEREICH, StringTool.getSafe(line, 6));
-	}
-	
-	private void fillImportedValues2018(String[] line){
 		importedValues.clear();
 		importedValues.put(Labor2009Tarif.FLD_CHAPTER, StringTool.getSafe(line, 0));
 		// convert code to nnnn.mm
 		String code = convertCodeString(StringTool.getSafe(line, 1));
-		
+
 		importedValues.put(Labor2009Tarif.FLD_CODE, code);
 		importedValues.put(Labor2009Tarif.FLD_TP,
-			convertLocalizedNumericString(StringTool.getSafe(line, 2)).toString());
-		importedValues.put(Labor2009Tarif.FLD_NAME,
-			StringTool.limitLength(StringTool.getSafe(line, 3), 254));
+				convertLocalizedNumericString(StringTool.getSafe(line, 2)).toString());
+		importedValues.put(Labor2009Tarif.FLD_NAME, StringTool.limitLength(StringTool.getSafe(line, 3), 254));
+		importedValues.put(Labor2009Tarif.FLD_LIMITATIO, StringTool.getSafe(line, 8));
+		String isChem = StringTool.getSafe(line, 11);
+		String isHaem = StringTool.getSafe(line, 12);
+		String isImm = StringTool.getSafe(line, 13);
+		String isGen = StringTool.getSafe(line, 14);
+		String isMikro = StringTool.getSafe(line, 15);
+		StringBuilder sb = new StringBuilder();
+		if (isChem.equals("Ja")) {
+			sb.append("C");
+		}
+		if (isHaem.equals("Ja")) {
+			sb.append("H");
+		}
+		if (isImm.equals("Ja")) {
+			sb.append("I");
+		}
+		if (isGen.equals("Ja")) {
+			sb.append("G");
+		}
+		if (isMikro.equals("Ja")) {
+			sb.append("M");
+		}
+
+		importedValues.put(Labor2009Tarif.FLD_FACHBEREICH, sb.toString());
+	}
+
+	private void fillImportedValues2012(String[] line) {
+		importedValues.clear();
+		importedValues.put(Labor2009Tarif.FLD_CHAPTER, StringTool.getSafe(line, 0));
+		// convert code to nnnn.mm
+		String code = convertCodeString(StringTool.getSafe(line, 2));
+
+		importedValues.put(Labor2009Tarif.FLD_CODE, code);
+		importedValues.put(Labor2009Tarif.FLD_TP,
+				convertLocalizedNumericString(StringTool.getSafe(line, 3)).toString());
+		importedValues.put(Labor2009Tarif.FLD_NAME, StringTool.limitLength(StringTool.getSafe(line, 4), 254));
+		importedValues.put(Labor2009Tarif.FLD_LIMITATIO, StringTool.getSafe(line, 5));
+		importedValues.put(Labor2009Tarif.FLD_FACHBEREICH, StringTool.getSafe(line, 7));
+	}
+
+	private void fillImportedValues2011(String[] line) {
+		importedValues.clear();
+		importedValues.put(Labor2009Tarif.FLD_CHAPTER, StringTool.getSafe(line, 0));
+		// convert code to nnnn.mm
+		String code = convertCodeString(StringTool.getSafe(line, 2));
+
+		importedValues.put(Labor2009Tarif.FLD_CODE, code);
+		importedValues.put(Labor2009Tarif.FLD_TP,
+				convertLocalizedNumericString(StringTool.getSafe(line, 3)).toString());
+		importedValues.put(Labor2009Tarif.FLD_NAME, StringTool.limitLength(StringTool.getSafe(line, 4), 254));
+		importedValues.put(Labor2009Tarif.FLD_LIMITATIO, StringTool.getSafe(line, 5));
+		importedValues.put(Labor2009Tarif.FLD_FACHBEREICH, StringTool.getSafe(line, 6));
+	}
+
+	private void fillImportedValues2018(String[] line) {
+		importedValues.clear();
+		importedValues.put(Labor2009Tarif.FLD_CHAPTER, StringTool.getSafe(line, 0));
+		// convert code to nnnn.mm
+		String code = convertCodeString(StringTool.getSafe(line, 1));
+
+		importedValues.put(Labor2009Tarif.FLD_CODE, code);
+		importedValues.put(Labor2009Tarif.FLD_TP,
+				convertLocalizedNumericString(StringTool.getSafe(line, 2)).toString());
+		importedValues.put(Labor2009Tarif.FLD_NAME, StringTool.limitLength(StringTool.getSafe(line, 3), 254));
 		importedValues.put(Labor2009Tarif.FLD_LIMITATIO, StringTool.getSafe(line, 4));
 		importedValues.put(Labor2009Tarif.FLD_FACHBEREICH, StringTool.getSafe(line, 5));
 	}
-	
-	private void closeAllOlder(){
+
+	private void closeAllOlder() {
 		// get all entries
 		TimeTool defaultValidFrom = new TimeTool();
 		defaultValidFrom.set(1970, 0, 1);
 		Query<Labor2009Tarif> qEntries = new Query<Labor2009Tarif>(Labor2009Tarif.class);
 		List<Labor2009Tarif> entries = qEntries.execute();
-		
+
 		for (Labor2009Tarif labor2009Tarif : entries) {
 			String validFromString = labor2009Tarif.get(Labor2009Tarif.FLD_GUELTIG_VON);
 			String validUntilString = labor2009Tarif.get(Labor2009Tarif.FLD_GUELTIG_BIS);
 			if ((validFromString == null || validFromString.trim().length() == 0)) {
 				// old entry with no valid from
-				labor2009Tarif.set(Labor2009Tarif.FLD_GUELTIG_VON,
-					defaultValidFrom.toString(TimeTool.DATE_COMPACT));
-				labor2009Tarif.set(Labor2009Tarif.FLD_GUELTIG_BIS,
-					validFrom.toString(TimeTool.DATE_COMPACT));
+				labor2009Tarif.set(Labor2009Tarif.FLD_GUELTIG_VON, defaultValidFrom.toString(TimeTool.DATE_COMPACT));
+				labor2009Tarif.set(Labor2009Tarif.FLD_GUELTIG_BIS, validFrom.toString(TimeTool.DATE_COMPACT));
 			} else if ((validUntilString == null || validUntilString.trim().length() == 0)
-				&& !validFrom.toString(TimeTool.DATE_COMPACT).equals(validFromString)) {
+					&& !validFrom.toString(TimeTool.DATE_COMPACT).equals(validFromString)) {
 				// old entry not closed yet
-				labor2009Tarif.set(Labor2009Tarif.FLD_GUELTIG_BIS,
-					validFrom.toString(TimeTool.DATE_COMPACT));
+				labor2009Tarif.set(Labor2009Tarif.FLD_GUELTIG_BIS, validFrom.toString(TimeTool.DATE_COMPACT));
 			}
 		}
 	}
 
-	private String convertCodeString(String code){
+	private String convertCodeString(String code) {
 		// split by all possible delimiters after reading for xls
 		String[] parts = code.split("[\\.,']");
 		StringBuilder sb = new StringBuilder();
@@ -344,16 +367,17 @@ public class EALReferenceDataImporter extends AbstractReferenceDataImporter {
 			sb.append("00");
 		else if (sb.length() == 6)
 			sb.append("0");
-		
+
 		return sb.toString();
 	}
-	
-	private String convertLocalizedNumericString(String localized){
+
+	private String convertLocalizedNumericString(String localized) {
 		DecimalFormat df = (DecimalFormat) NumberFormat.getInstance();
 		Number number = new Integer(0);
 		try {
 			number = df.parse(localized);
-		} catch (ParseException pe) { /* ignore and return default 0 */}
+		} catch (ParseException pe) {
+			/* ignore and return default 0 */}
 		// cut off decimals if there are none
 		if ((number.doubleValue() % 1.0) > 0) {
 			return Double.toString(number.doubleValue());
@@ -362,7 +386,7 @@ public class EALReferenceDataImporter extends AbstractReferenceDataImporter {
 		}
 	}
 
-	private int getFormatYear(String[] line){
+	private int getFormatYear(String[] line) {
 		String fach2011 = StringTool.getSafe(line, 6);
 		String fach2012 = StringTool.getSafe(line, 7);
 		String code2018 = StringTool.getSafe(line, 1);
@@ -374,19 +398,21 @@ public class EALReferenceDataImporter extends AbstractReferenceDataImporter {
 			}
 		} else if (fach2011.equals("") && !fach2012.equals("")) {
 			return 2012;
+		} else if (fach2011.equals("Resultat")) {
+			return 2023;
 		}
 		return -1;
 	}
-	
-	private boolean isCode(String code2018){
+
+	private boolean isCode(String code2018) {
 		code2018 = code2018.replaceAll("[\\.,']", "");
 		Integer value = Integer.valueOf(code2018);
 		return value >= 1000;
 	}
 
 	@Override
-	public int getCurrentVersion(){
+	public int getCurrentVersion() {
 		return Labor2009Tarif.getCurrentCodeVersion();
 	}
-	
+
 }
